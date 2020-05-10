d037cc527e9e43b92f05a136a9c76d441a6dcf6c
Maythee Anegboonlap <null@llun.me>
2020-05-10 15:40:13 +0800

    Format code with prettier

--- _11ty/getTagList.js
+++ _11ty/getTagList.js
@@ -1,28 +1,28 @@
-module.exports = function(collection) {
-  let tagSet = new Set()
-  collection.getAll().forEach(function(item) {
-    if ('tags' in item.data) {
-      let tags = item.data.tags
+module.exports = function (collection) {
+  let tagSet = new Set();
+  collection.getAll().forEach(function (item) {
+    if ("tags" in item.data) {
+      let tags = item.data.tags;
 
-      tags = tags.filter(function(item) {
+      tags = tags.filter(function (item) {
         switch (item) {
           // this list should match the `filter` list in tags.njk
-          case 'all':
-          case 'nav':
-          case 'post':
-          case 'posts':
-            return false
+          case "all":
+          case "nav":
+          case "post":
+          case "posts":
+            return false;
         }
 
-        return true
-      })
+        return true;
+      });
 
       for (const tag of tags) {
-        tagSet.add(tag)
+        tagSet.add(tag);
       }
     }
-  })
+  });
 
   // returning an array in addCollection works in Eleventy 0.5.3
-  return [...tagSet]
-}
+  return [...tagSet];
+};
