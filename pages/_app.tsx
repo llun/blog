0c0e749d4684070fd338aa911755fb568f5b1735
Maythee Anegboonlap <null@llun.me>
2021-11-09 08:59:44 +0800

    Move static files into public

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../css/index.css'
+import '../public/css/index.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
