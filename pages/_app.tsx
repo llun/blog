1d158c0fc3c1371523c0c2d9f39d37d8e024d0b1
Maythee Anegboonlap <contact@llun.dev>
2022-10-26 08:56:16 +0200

    Add React Modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import ReactModal from 'react-modal'
 import '../public/css/index.css'
+
+ReactModal.setAppElement('#__next')
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { GetStaticPropsContext } from 'next'
-
 import Link from 'next/link'
+import React from 'react'
 
 import {
   Post,
