bd02026b98c3ff7c5b2547465b4a210daffdb0df
Maythee Anegboonlap <contact@llun.dev>
2023-10-30 19:03:01 +0100

    Add All posts page to AppRouter

---
+++ app/posts/page.tsx
@@ -0,0 +1,32 @@
+import { Metadata } from 'next'
+import React from 'react'
+
+import { getMetadata } from '../../components/Meta'
+import PostList from '../../components/PostList'
+import {
+  getAllPosts,
+  getConfig,
+  postDescendingComparison
+} from '../../libs/blog'
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  const { url, title, description } = getConfig()
+  return getMetadata({
+    url,
+    title: `${title}, all posts`,
+    description
+  })
+}
+
+const Posts = () => {
+  const posts = getAllPosts().sort(postDescendingComparison)
+
+  return (
+    <main>
+      <h2>All Posts</h2>
+      <PostList posts={posts} />
+    </main>
+  )
+}
+
+export default Posts
